8b59753e009b234078da70074485b1692e7ca3df
ivoesc <ivoescoli@gmail.com>
2022-02-07 21:28:33 -0300

    Usando express-generator para crear carpetas del MVC

--- app.js
+++ app.js
@@ -1,3 +1,44 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+/* 
 const express = require('express');
 const app = express();
 const path = require('path');
@@ -6,9 +47,9 @@ const publicPath = path.resolve(__dirname, './public');
 app.use(express.static(publicPath));
 
 
-app.listen(4000, () => {
-    console.log('Servidor funcionando');
-});
+app.listen (process.env.PORT || 3000, function () {
+    console.log ("Servidor corriendo en el puerto 3000");
+})
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/views/home.html');
@@ -22,10 +63,6 @@ app.get('/register', (req, res) => {
     res.sendFile(__dirname + '/views/register.html');
 });
 
-app.get('/movie-detail', (req, res) => {
-    res.sendFile(__dirname + '/views/movie-detail.html');
-});
+*/
 
-app.get('/carrito', (req, res) => {
-    res.sendFile(__dirname + '/views/carrito.html');
-});
+module.exports = app;
