68af4742676e5c195da4a792a66839fc19368c03
ivoesc <ivoescoli@gmail.com>
2022-02-22 19:12:53 -0300

    Modificando el app

--- app.js
+++ app.js
@@ -1,16 +1,8 @@
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
-// const cookieParser = require('cookie-parser');
-// const logger = require('morgan');
-
-/* const indexRouter = require('./src/routes/indexRoutes');
-const loginRouter = require('./src/routes/loginRoutes');
-const registerRouter = require('./src/routes/registerRoutes');
-const carritoRouter = require('./src/routes/carritoRoutes');
-const moviedetailRouter = require('./src/routes/movie-detailRoutes');
-const usersRouter = require('./src/routes/usersRoutes');
-*/
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
 const app = express();
 
@@ -22,10 +14,10 @@ app.listen (process.env.PORT || 3000, function () {
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// app.use(logger('dev'));
+app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-// app.use(cookieParser());
+app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // ************ Route System require and use() ************
