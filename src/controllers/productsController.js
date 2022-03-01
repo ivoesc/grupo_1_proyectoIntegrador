f78decbbd6f8a305f2498658d8e61eb8d2f09ee3
manuyeregui <manuelyeregui@gmail.com>
2022-03-01 03:35:13 -0300

    arreglando logica de multer de edit

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -34,12 +34,9 @@ const productsController = {
 		productToCreate.price = Number(req.body.price);
         productToCreate.duration = Number(req.body.duration);
 		
-		
 		productToCreate.image = req.files.image[0].filename;
 		productToCreate.background = req.files.background[0].filename;
 
-		console.log(req.files);
-		console.log(req.files.background[0].backgroundFilename);
 		//asignar id
 		productToCreate.id = productsController.asignarIdProducto(productToCreate);
 		const ultimoElementoDeArray = products [products.length - 1];
@@ -52,8 +49,6 @@ const productsController = {
 
 		return res.send(products);
     },
-
-    
     
 	asignarIdProducto(productToCreate) {
 		return products[products.length - 1].id +1;
@@ -70,13 +65,16 @@ const productsController = {
     },
 
     update: (req, res) => {
-        const idProducto = req.params.id;
-		const indiceDelProducto = products.findIndex((p) => p.id == idProducto);
-
-		const productToEdit = (req.body);
+        const productToEdit = (req.body);
 
 		productToEdit.price = Number(req.body.price);
         productToEdit.duration = Number(req.body.duration);
+
+		productToEdit.image = req.files.image[0].filename;
+		productToEdit.background = req.files.background[0].filename;
+
+		const idProducto = req.params.id;
+		const indiceDelProducto = products.findIndex((p) => p.id == idProducto);
 
 		products[indiceDelProducto] = {...products[indiceDelProducto], ...req.body};
 
